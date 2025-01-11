dd99a7218b3040160e13c1a41da9b274d2dd27b3
basar <basarsubasi@protonmail.com>
2025-01-11 20:07:17 +0300

    1.1.4

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -68,7 +68,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: 20,
-    paddingTop: 80,
+    paddingTop: 40,
   },
   headerContainer: {
     marginBottom: 20,
