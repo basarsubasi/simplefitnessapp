aa78cbb04187f95173903f0983abf3810248cb16
basar <basarsubasi@protonmail.com>
2025-05-18 04:29:54 +0300

    cjk system fallback

--- App.tsx
+++ App.tsx
@@ -44,7 +44,6 @@
   import { AppState } from 'react-native';
   import GraphsWorkoutSelection from './screens/GraphsWorkoutSelection';
   import GraphsWorkoutDetails from './screens/GraphsWorkoutDetails';
-  import { FontProvider } from './context/FontContext';
 
 
 
@@ -532,11 +531,9 @@ const AppContent = () => {
       <GestureHandlerRootView>
         <NavigationContainer ref={navigationRef}>
           <SettingsProvider>
-          <FontProvider>
           <I18nextProvider i18n={i18n}>
           <AppContent/>
           </I18nextProvider>
-          </FontProvider>
           </SettingsProvider>
          
         </NavigationContainer>
