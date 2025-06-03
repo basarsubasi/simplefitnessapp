2ce7a7a638656368c2580a7b4e4a6bd101e3b43a
basar <basarsubasi@protonmail.com>
2025-06-03 18:43:53 +0300

    test data

--- App.tsx
+++ App.tsx
@@ -479,7 +479,7 @@ const AppContent = () => {
     React.useEffect(() => {
       (async () => {
         try {
-          //await resetDatabase();
+          // await resetDatabase();
           await loadDatabase();
           setDbLoaded(true);
         } catch (e) {
