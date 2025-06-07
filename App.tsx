1dbcba0554c5408b398df2d2875022f355404b29
basar <basarsubasi@protonmail.com>
2025-06-07 03:48:28 +0300

    scheduling within the calendar

--- App.tsx
+++ App.tsx
@@ -141,7 +141,7 @@
 
   export type WorkoutLogStackParamList = {
     MyCalendar: undefined; // No parameters for this route
-    LogWorkout: undefined; // No parameters for this route
+    LogWorkout: { selectedDate?: string };
     RecurringWorkoutOptions: undefined;
     CreateRecurringWorkout: undefined;
     ManageRecurringWorkouts: undefined;
