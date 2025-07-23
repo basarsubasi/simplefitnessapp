4005a30ac0fcca93670bdc37c735513b48b5529d
basar <basarsubasi@protonmail.com>
2025-07-23 15:33:06 +0300

    statusbar dim

--- screens/MyCalendar.tsx
+++ screens/MyCalendar.tsx
@@ -9,6 +9,7 @@ import {
   Modal,
   ViewStyle,
   Platform,
+  StatusBar,
 } from 'react-native';
 import {
   useFocusEffect,
@@ -825,6 +826,13 @@ export default function MyCalendar() {
           setDetailedWorkout(null);
         }}
       >
+         {modalVisible && (
+          <StatusBar
+            backgroundColor={theme.type === 'light' ? "rgba(0, 0, 0, 0.5)" : "black"}
+            barStyle={theme.type === 'light' ? 'light-content' : 'dark-content'}
+          />
+        )}
+        
         <View
           style={[
             styles.modalContainer,
